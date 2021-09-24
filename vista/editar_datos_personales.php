42ad24383dea072893012f0ff2c57f7cb686fa62
rosa vallejos guillen <vallejosguillenrosa23@gmail.com>
2021-09-24 02:14:27 +0200

    modificar datos personales

--- vista/editar_datos_personales.php
+++ vista/editar_datos_personales.php
@@ -3,7 +3,7 @@ session_start();
 if ($_SESSION['us_tipo']==1||$_SESSION['us_tipo']==3){
 	include_once 'layouts/header.php';
 ?>
-	  <title>Adm|Editar Datos</title>
+	  <title>Adm|Editar Datos personales</title>
 	  <!-- Tell the browser to be responsive to screen width -->
 		<?php 
 			include_once 'layouts/nav.php';
